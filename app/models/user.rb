4b6b024d10d52f32e5130259f980457a5d42592f
Samuel <23000349+sassani134@users.noreply.github.com>
2025-08-07 21:17:44 +0200

    Create roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [ user: 0, admin: 1 ]
+  after_initialize :set_default_role, if: :new_record?
+
+  private
+
+  def set_default_role
+    self.role ||= :user # Set default role to 'user' if not specified
+  end
 end
