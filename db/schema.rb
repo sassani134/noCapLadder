083f0178822d1a9fe8d3e1d09bf234d721726c27
Samuel <23000349+sassani134@users.noreply.github.com>
2025-08-08 08:31:41 +0200

    create role for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_07_180208) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_07_192324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "participations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "party_id", null: false
+    t.boolean "winner"
+    t.boolean "loser"
+    t.boolean "ventre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["party_id"], name: "index_participations_on_party_id"
+    t.index ["user_id"], name: "index_participations_on_user_id"
+  end
+
+  create_table "parties", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,9 +39,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_07_180208) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "role", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "participations", "parties"
+  add_foreign_key "participations", "users"
 end
