985545b7259a10afc5fc8bd97e8c3c72404e2132
Samuel <23000349+sassani134@users.noreply.github.com>
2025-08-08 08:32:12 +0200

    create party model

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,2 +1,7 @@
 class Party < ApplicationRecord
+  # Une participation appartient à une Party
+  has_many :participations
+
+  # Une Party a plusieurs utilisateurs à travers les participations
+  has_many :users, through: :participations
 end
