be5c16b15c19321d76ea84bd766b78e128adba79
Samuel <23000349+sassani134@users.noreply.github.com>
2025-08-08 08:30:39 +0200

    correct user role enum syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: [ user: 0, admin: 1 ]
+  enum :role, { user: 0, admin: 1 }
   after_initialize :set_default_role, if: :new_record?
+
+  # Une participation appartient à un User
+  has_many :participations
+
+  # Un User a plusieurs parties à travers les participations
+  has_many :parties, through: :participations
 
   private
 
